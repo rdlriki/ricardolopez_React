5fb650575bdce8fa4a160a1f2ec2e242e44ee05d
rdlriki <lopez_ricardo1980@hotmail.com>
2022-06-11 14:49:12 -0300

    first commit

---
+++ src/SaludoAlPie.js
@@ -0,0 +1,11 @@
+function SaludoAlPie(props) {
+    return (
+    <div className="SaludoAlPie">
+        <p>
+            Hola {props.nombre}, estoy saludando en el pie!. 
+        </p>
+    </div>
+    );
+}
+
+export default SaludoAlPie;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import SaludoAlPie from './SaludoAlPie';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <SaludoAlPie nombre = 'Ana Maria'/> 
   </React.StrictMode>
 );
 
@@ -15,3 +17,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+    
